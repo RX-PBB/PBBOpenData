5f0d0ecfcb0e062aed682cc14d6b60aeea6c2f09
ResourceX <mseman@resourcex.net>
2018-05-26 18:50:51 -0400

    adding feedback function

---
+++ R/submitFeedback.R
@@ -0,0 +1,33 @@
+#' A Cat Function
+#'
+#' This function allows you to express your love of cats.
+#' @param name Sender's name
+#' @param email Sender's email
+#' @param comment Email's content, the feedback they submitted
+#' @examples
+#' submitFeedback()
+#'
+submitFeedback<-function(name=input$name,email=input$email,comment=input$comment){
+
+  body<-paste("<p><strong>",name,"</strong> has sent feedback from our PBB open data site.</p>
+                        <p>",name," has provided the following email: ",email,"</p>
+                        <p> Comments: </p>
+                        <p>",comment," </p>",sep='')
+
+        for(i in 1:length(emails)){
+        send.mail(from = "helpdesk@resourcex.net",
+                    to = emails[i],
+                    subject = Subject,
+                    body = body,
+                    html=T,
+                    smtp = list(host.name = "smtp.gmail.com", port = 465, user.name = "helpdesk@resourcex.net", passwd = "elonmusk2017", ssl = TRUE),
+                    authenticate = T,
+                    send = TRUE)
+        }
+
+        showModal(modalDialog(
+            size='s',easyClose = T,
+            h3('Thanks for your feedback!')
+        ))
+
+}
