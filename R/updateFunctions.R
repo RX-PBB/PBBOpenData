ab90b859b8762fab028b7bf75b62d67352b9f82e
ResourceX <mseman@resourcex.net>
2018-05-28 19:16:28 -0400

    adding update function

---
+++ R/updateFunctions.R
@@ -0,0 +1,45 @@
+#**************************************************
+#
+#  Functions for Updating Data
+#
+#**************************************************
+
+
+#' update_Tabs
+#'
+#' updates the tabset
+#' @param tabset input selector
+#' @param bpas path to bpas tabs json file
+#' @param results path to results tabs json file
+#' @export
+#' @examples
+#' update_Tabs(tabset=input$tabset,bpas='./www/config/roswell_bpas.json',results='./www/config/roswell_tabs.json')
+
+
+update_Tabs<-function(tabset,bpas,results){
+
+  if (is.null(tabset)){
+     data<-fromJSON(file=results)
+     session$sendCustomMessage(type="updatetabs",toJSON(data))
+  }
+
+
+  if (tabset=='Program Attributes'){
+
+     data<-fromJSON(file=bpas)
+     session$sendCustomMessage(type="updatetabs",toJSON(data))
+
+     #session$sendCustomMessage(type="updatetabs_height",160)
+
+  }
+
+  if (tabset=='Results'){
+
+     data<-fromJSON(file=results)
+     session$sendCustomMessage(type="updatetabs",toJSON(data))
+
+     #session$sendCustomMessage(type="updatetabs_height",235)
+
+  }
+
+}
