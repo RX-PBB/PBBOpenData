040b86c6df9c4d7f37f0e0dcecc82d66c76e4ce8
resourcex <mseman@resourcex.net>
2018-11-21 21:45:47 -0500

    added table function

---
+++ R/makeProgramResultsTable.R
@@ -0,0 +1,62 @@
+#' make_ProgramResultsTable
+#'
+#' Create a DT table of the pbb data for the chart tabs
+#' @param df present pbb data
+#' @param buttons what buttons to use
+#' @export
+#' @examples
+#' make_ProgramResultsTable(df,buttons=c('copy', 'excel', 'print'))
+
+
+
+make_ProgramResultsTable<-function(df,buttons=c('copy', 'excel', 'print')){
+
+  df[["More Info"]]<-
+    paste0('
+           <div class="btn-group" role="group" aria-label="Basic example">
+           <button type="button" class="btn btn-default action-button" id=ProgID_',df$`#`,'>More Info</button>
+
+
+           </div>
+
+           ')
+
+
+  df<-df[c('More Info',input$tabdata_dataField,'DirectCost', 'Program','Description')]
+  df[,input$tabdata_dataField]<-as.numeric(df[,input$tabdata_dataField])
+  df<-df[order(-df[,input$tabdata_dataField],-df$DirectCost),]
+  df$DirectCost<-format(round(df$DirectCost,digits=0),big.mark = ",")
+
+  height<-600
+
+  width<-1100
+
+  dt<-datatable(df,escape=F,selection = 'none',rownames=F,extensions = list(Scroller=NULL,Buttons=NULL),
+                options = list(stateSave=T,
+                               dom = 'Bfrtip',
+                               buttons = buttons,
+                               searchHighlight = TRUE,
+                               autoWidth=TRUE,
+                               columnDefs = list(list(width =paste(width*.1,'px',sep=''), targets = c(0)),
+                                                 list(width =paste(width*.08,'px',sep=''), targets = c(1)),
+                                                 list(width =paste(width*.08,'px',sep=''), targets = c(2)),
+                                                 list(width =paste(width*.25,'px',sep=''), targets = c(3)),
+                                                 list(width =paste(width*.44,'px',sep=''), targets = c(4)),
+                                                 list(className='dt-center',targets=c(0:2))
+                                                 #list(width ='30%', targets = 1),
+                                                 #list(className = 'dt-center', targets = c(0,1,2,3,4,6,7,8))
+                               ),
+                               deferRender=TRUE,
+                               scrollX=FALSE,
+                               scrollY=height,
+                               scrollCollapse=TRUE,
+                               pageLength = 25, lengthMenu = c(25,50,150,250)
+
+                ))
+
+
+
+
+
+  return(dt)
+}
