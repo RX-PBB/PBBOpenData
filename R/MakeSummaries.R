b7c68fa76f71bf0fa3d7ce7ed480a756e888f539
ResourceX <mseman@resourcex.net>
2018-12-03 15:23:44 -0500

    updated find replace of illegal characters for tree map

--- R/MakeSummaries.R
+++ R/MakeSummaries.R
@@ -158,7 +158,7 @@ makeOpenPBBData_Summaries<-function(db_name_new,db_host_new,BudgetID,CostModelID
     temp[temp$Quartile>2 & temp$Reliance<2 & temp$Mandate<2 & !is.na(temp$Mandate),'Policy5']<-4
 
     #screen for :,;,! our of Department, Division, and Program fields
-    for (i in 3:5){
+    for (i in 3:6){
       temp[,i]<-gsub(":","-",temp[,i])
       temp[,i]<-gsub(";","-",temp[,i])
       temp[,i]<-gsub("!","",temp[,i])
