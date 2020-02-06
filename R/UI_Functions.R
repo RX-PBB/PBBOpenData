83d8fb649df2e0914ea8d5103dfccb55220e800a
resourcex <mseman@resourcex.net>
2020-02-05 21:36:41 -0500

    updates for flagstaff and salem - table and treemap legend

--- R/UI_Functions.R
+++ R/UI_Functions.R
@@ -167,9 +167,13 @@ app_header<-function(header='header.jpg',info.top=245,info.left=20,header_logo=N
 #' @export
 #' @examples
 #' app_charts()
-app_charts<-function(results_tab=NULL,results_chart_height=500,include_legend=NULL,hasMetrics=NULL){
+app_charts<-function(results_tab=NULL,results_chart_height=500,hasMetrics=NULL){
 
+  include_legend<-treemap$include_legend
+  legend_position<-treemap$legend_position
   treemap_legend<-NULL
+
+
   if(!is.null(include_legend)){
 
     if(include_legend==TRUE){
@@ -182,6 +186,35 @@ app_charts<-function(results_tab=NULL,results_chart_height=500,include_legend=NU
     #default other clients to always include unless we come back and specify false
     #but for now test as default no on if not specified
     #treemap_legend<-uiOutput('treemap_legend')
+
+    treemap_legend<-NULL
+  }
+
+  legend_top<-NULL
+  legend_bottom<-NULL
+
+  if(!is.null(legend_position)){
+
+    if(legend_position=='top'){
+
+      legend_top<-HTML(paste0('<h4>
+          <em>In the plot below, each box represents a program. The size of the box shows cost.
+              Box shade indicates how significant the program is to achieving the result.
+              The darker the shade the more aligned the program is with the selected result.</em>
+        </h4>',treemap_legend))
+
+
+    }else{
+
+     legend_bottom<-HTML(paste0(treemap_legend,'<h4>
+          <em>In the plot above, each box represents a program. The size of the box shows cost.
+              Box shade indicates how significant the program is to achieving the result.
+              The darker the shade the more aligned the program is with the selected result.</em>
+        </h4>'))
+
+    }
+
+
   }
 
 
@@ -195,12 +228,13 @@ app_charts<-function(results_tab=NULL,results_chart_height=500,include_legend=NU
 
                 shiny::tabPanel('TreePlot',
 
-                          h4(em("Click into the boxes below for more detail, you can click down to view details on Departments, Divisions, and Programs. Click \"Overall\" to return to the top.")),
+                             h4(em("Click into the boxes below for more detail, you can click down to view details on Departments, Divisions, and Programs. Click \"Overall\" to return to the top.")),
 
+                             legend_top,
 
-                         tags$div(id="treemap", style="width:1100px; height:700px; overflow:hidden"),
+                              tags$div(id="treemap", style="width:1100px; height:700px; overflow:hidden"),
 
-                         treemap_legend
+                              legend_bottom
 
 
                 ),
@@ -241,14 +275,14 @@ app_charts<-function(results_tab=NULL,results_chart_height=500,include_legend=NU
                              tags$div(id="stackedbar", style=paste0("width:1100px; height:",results_chart_height,"px; overflow:hidden"))
                     ),
                     shiny::tabPanel('TreePlot',
-                             h4(em("Click into the boxes below for more detail, you can click down to view details on Departments, Divisions, and Programs. Click \"Overall\" to return to the top.")),
 
-                             h4(em("In the plot below, each box represents a program. The size of the box shows cost. Box shade indicates how significant the program is to achieving the result. The darker the shade the more aligned the program is with the selected result.")),
+                              h4(em("Click into the boxes below for more detail, you can click down to view details on Departments, Divisions, and Programs. Click \"Overall\" to return to the top.")),
 
+                              legend_top,
 
-                             tags$div(id="treemap", style="width:1100px; height:700px; overflow:hidden"),
+                              tags$div(id="treemap", style="width:1100px; height:700px; overflow:hidden"),
 
-                             treemap_legend
+                              legend_bottom
 
 
                     ),
--- R/makeProgramResultsTable.R
+++ R/makeProgramResultsTable.R
@@ -10,7 +10,6 @@
 #' make_ProgramResultsTable(df,tabdata_dataField=input$tabdata_dataField, buttons=c('copy', 'excel', 'print'))
 
 
-
 make_ProgramResultsTable<-function(df,tabdata_dataField,buttons=c('copy', 'excel', 'print'), doneFun=NULL,height=600){
 
   df[["More Info"]]<-
@@ -30,6 +29,45 @@ make_ProgramResultsTable<-function(df,tabdata_dataField,buttons=c('copy', 'excel
   df$DirectCost<-format(round(df$DirectCost,digits=0),big.mark = ",")
 
   #height<-600
+
+
+  if(tabdata_dataField=='Overall'){
+
+    #Relabel Overall in terms of alignment
+    if(!is.null(table$overall_column)){
+
+      overall_column<-table$overall_column
+      overall_labels<-table$overall_labels
+
+      colnames(df)[colnames(df)=='Overall']<-overall_column
+      df[df[,overall_column]==4 & !is.na(df[,overall_column]),overall_column]<-overall_labels[6]
+      df[df[,overall_column]==3 & !is.na(df[,overall_column]),overall_column]<-overall_labels[5]
+      df[df[,overall_column]==2 & !is.na(df[,overall_column]),overall_column]<-overall_labels[4]
+      df[df[,overall_column]==1 & !is.na(df[,overall_column]),overall_column]<-overall_labels[3]
+      df[df[,overall_column]==0 & !is.na(df[,overall_column]),overall_column]<-overall_labels[2]
+      df[is.na(df[,overall_column]),overall_column]<-overall_labels[1]
+
+    }
+
+  }else{
+
+    if(!is.null(table$result_labels)){
+
+      result_labels<-table$result_labels
+
+      df[df[,tabdata_dataField]==4 & !is.na(df[,tabdata_dataField]),tabdata_dataField]<-result_labels[6]
+      df[df[,tabdata_dataField]==3 & !is.na(df[,tabdata_dataField]),tabdata_dataField]<-result_labels[5]
+      df[df[,tabdata_dataField]==2 & !is.na(df[,tabdata_dataField]),tabdata_dataField]<-result_labels[4]
+      df[df[,tabdata_dataField]==1 & !is.na(df[,tabdata_dataField]),tabdata_dataField]<-result_labels[3]
+      df[df[,tabdata_dataField]==0 & !is.na(df[,tabdata_dataField]),tabdata_dataField]<-result_labels[2]
+      df[is.na(df[,tabdata_dataField]),tabdata_dataField]<-result_labels[1]
+
+
+    }
+
+
+  }
+
 
   width<-1100
 
@@ -53,10 +91,11 @@ make_ProgramResultsTable<-function(df,tabdata_dataField,buttons=c('copy', 'excel
                                scrollX=FALSE,
                                scrollY=height,
                                scrollCollapse=TRUE,
-                               pageLength = 25, lengthMenu = c(25,50,150,250)
+                               pageLength = 150, lengthMenu = c(25,50,150,250)
 
 
                 ))
   return(dt)
 }
+
 
